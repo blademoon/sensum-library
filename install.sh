ad100a66d0cf99818ae2284eaf6db391d67b4b70
ArtyomBoyko <blademoon@yandex.ru>
2017-12-24 21:27:32 +0500

    Минимально необходимый набор действий в случаях: - Если в дистрибутиве есть папка /usr/local/include   - если уже есть старая версия библиотеки.   - если это свежая установка.

--- install.sh
+++ install.sh
@@ -1,10 +1,41 @@
 #!/bin/bash
 
-$DIRECTORY = /usr/local/include/
+LOCAL_INCLUDE_DIRECTORY="/usr/local/include/"
+SENSUM_LIB_DIRECTORY="/usr/local/include/sensum"
+SENSUM_EXAMPLES_PATH="/usr/src/sensum-library-examples"
 
-if [ -d "$DIRECTORY" ]; then
+if [ -d "$LOCAL_INCLUDE_DIRECTORY" ]; then
+	echo "Local include directory exist, starting installation..."  
+	
+	if [ -d "$SENSUM_LIB_DIRECTORY" ]; then
+		echo "Old library folders exist. Deleting..."
+		rm -rf $SENSUM_LIB_DIRECTORY
+	fi
+	
+	if [ -d "$SENSUM_EXAMPLES_PATH" ]; then
+                echo "Old library examples folders exist. Deleting..."
+                rm -rf $SENSUM_LIB_DIRECTORY
+	fi
+	
+	echo "Creating library folder..."
+	mkdir "$SENSUM_LIB_DIRECTORY"
 
-	echo "Local include folder exist, starting installation."  
-	mkdir $DIRECTORY/sensum
-	cp -v ./lib/* $DIRECTORY/sensum/
+	echo "Copy library files..."
+	cp -v ./lib/* "$SENSUM_LIB_DIRECTORY"
+
+	echo "Creating library examples folder..."
+	mkdir "$SENSUM_EXAMPLES_PATH"
+
+	echo "Copy library examples files..."
+	cp -v ./examples/* "$SENSUM_EXAMPLES_PATH"
+	chmod +x "$SENSUM_EXAMPLES_PATH/compile.sh"
 fi
+
+if [ ! -d "LOCAL_INCLUDE_DIRECTORY" ]; then
+	echo "Local include directory does not exist, starting installation..."
+fi
+
+
+
+echo "The library was copied to the folder $SENSUM_LIB_DIRECTORY"
+echo "Examples of using the library copied to a folder $SENSUM_EXAMPLES_PATH"

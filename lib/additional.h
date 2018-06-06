499f375fe6c079d0f22426f7fea410ec6d9cd453
Artyom Boyko <blademoon@yandex.ru>
2018-06-06 11:50:43 +0500

    Исправил объявление функции cur_time.
    Добавление функционала по возврату текущей секунды.

--- lib/additional.h
+++ lib/additional.h
@@ -4,7 +4,7 @@
 // Объявление функций библиотеки Sensus additional.
 
 // Функция возвращяющая текущий час. Зависит от настроек NTP!
-int cur_time(int &current_hour, int &current_minute,int &current_week_day);		
+int cur_time(int &current_hour,int &current_minute, int &current_second, int &current_week_day);		
 
 // Функция получения текущей даты в корректном для MySQL формате.
 int cur_date(std::string &current_day, std::string &current_month, std::string &current_year); 

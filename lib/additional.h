ae1b7bdb24ff3809f7d79ce424ca7560d7f06c2d
Artyom Boyko <blademoon@yandex.ru>
2018-06-06 11:44:00 +0500

    Поправил оформление.

--- lib/additional.h
+++ lib/additional.h
@@ -2,8 +2,12 @@
 #define SENSUM_ADDITIONAL_H
 
 // Объявление функций библиотеки Sensus additional.
-int cur_time(int &current_hour, int &current_minute,int &current_week_day);		// Функция возвращяющая текущий час. Зависит от настроек NTP!
-int cur_date(std::string &current_day,std::string &current_month,std::string &current_year); // Функция получения текущей даты в корректном для MySQL формате.
+
+// Функция возвращяющая текущий час. Зависит от настроек NTP!
+int cur_time(int &current_hour, int &current_minute,int &current_week_day);		
+
+// Функция получения текущей даты в корректном для MySQL формате.
+int cur_date(std::string &current_day, std::string &current_month, std::string &current_year); 
 
 //  Значения возвращаемые функциями
 #define	FUNCTION_SUCCESS    1                                                   // Функция выполнена успешно. The function completed successfully.
